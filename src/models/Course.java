7a2338410ca8da250b4e06191f9127b2ecd253bc
Fabio Teixeira <fabio1079@gmail.com>
2014-04-20 22:05:48 -0300

    Created DAO for Course

--- src/models/Course.java
+++ src/models/Course.java
@@ -1,9 +1,9 @@
 package models;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Hashtable;
 
-import libraries.GenericCRUDModel;;
+import models.DAO.CourseDAO;
 
 public class Course {
 	private Integer id;
@@ -35,178 +35,44 @@ public class Course {
 	}
 
 
-	public boolean save() {
-		boolean result = false;
-
-		Hashtable<String, String> data = new Hashtable<String, String>();
-		data.put("name", this.getName());
-
-		try {
-			GenericCRUDModel crud = new GenericCRUDModel();
-
-			if( this.id > 0 )
-				result = crud.update("course", data, this.id);
-			else {
-				result = crud.insert("course", data);
-
-				if (result)
-					this.id = Course.last().getId();
-			}
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-
-		return result;
-	}
-
-	public boolean delete() {
-		boolean result = false;
-
-		try {
-			GenericCRUDModel crud = new GenericCRUDModel();
-
-			if( this.id > 0 ) {
-				result = crud.delete("course", this.id);
-				this.id = 0;
-			}
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-
-		return result;
+	public boolean save() throws ClassNotFoundException, SQLException {
+		CourseDAO cDB = new CourseDAO();
+		return cDB.insert(this);
 	}
 
 
-	public static ArrayList<String> fieldsList() {
-		ArrayList<String> fields = new ArrayList<String>();
-		
-		fields.add("id");
-		fields.add("name");
-
-		return fields;
+	public static Course get(int id) throws ClassNotFoundException, SQLException {
+		CourseDAO cDB = new CourseDAO();
+		return cDB.getCourse(id);
 	}
 
 
-	public static Course get(int id) {
-		Course course = null;
-
-		try {
-			GenericCRUDModel crud = new GenericCRUDModel();
-			ArrayList<Hashtable<String, String>> data = crud.select("course", Course.fieldsList(), id);
-
-			if( data.size() == 1 ) {
-				String idcourse = data.get(0).get("id");
-				String namecourse = data.get(0).get("name");
-
-				course = new Course(Integer.parseInt(idcourse), namecourse);
-			}
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-
-		return course;
-	}
-
-	public static ArrayList<Course> getAll() {
-		ArrayList<Course> result = new ArrayList<Course>();
-
-		try {
-			GenericCRUDModel crud = new GenericCRUDModel();
-
-			ArrayList<Hashtable<String, String>> selectRsult = crud.select("course", Course.fieldsList());
-
-			if( selectRsult.size() > 0 ) {
-				Hashtable<String, String> row;
-
-				for(int i = 0; i < selectRsult.size(); i++) {
-					row = selectRsult.get(i);
-
-					result.add(new Course(Integer.parseInt(row.get("id")), row.get("name")));
-				}
-			}
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-
-		return result;
+	public static ArrayList<Course> getAll() throws ClassNotFoundException, SQLException {
+		CourseDAO cDB = new CourseDAO();
+		return cDB.selectAll();
 	}
 
 
-	public static int count() {
-		int count = 0;
-
-		try {
-			GenericCRUDModel crud = new GenericCRUDModel();
-			count = crud.count("course");
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-
-		return count;
+	public static int count() throws ClassNotFoundException, SQLException {
+		CourseDAO cDB = new CourseDAO();
+		return cDB.count();
 	}
 
 
-	public static Course first() {
-		Course course = null;
-
-		try {
-			GenericCRUDModel crud = new GenericCRUDModel();
-
-			Hashtable<String, String> row = crud.firstOrLast("course", Course.fieldsList(), false);
-
-			course = new Course(Integer.parseInt(row.get("id")), row.get("name"));
-
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		return course;
+	public static Course first() throws ClassNotFoundException, SQLException {
+		CourseDAO cDB = new CourseDAO();
+		return cDB.firstOrLast(false);
 	}
 
 
-	public static Course last() {
-		Course course = null;
-
-		try {
-			GenericCRUDModel crud = new GenericCRUDModel();
-
-			Hashtable<String, String> row = crud.firstOrLast("course", Course.fieldsList(), true);
-
-			course = new Course(Integer.parseInt(row.get("id")), row.get("name"));
-
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		return course;
+	public static Course last() throws ClassNotFoundException, SQLException {
+		CourseDAO cDB = new CourseDAO();
+		return cDB.firstOrLast(true);
 	}
 
 
-	public static ArrayList<Course> getWhere(String key, String value, boolean like) {
-		ArrayList<Course> result = new ArrayList<Course>();
-		String conditions;
-
-		if( like == true )
-			conditions = key+" LIKE '%"+value+"%'";
-		else
-			conditions = key+" = '"+value+"'";
-
-		try {
-			GenericCRUDModel crud = new GenericCRUDModel();
-
-			ArrayList<Hashtable<String, String>> selectRsult = crud.selectWhere("course", Course.fieldsList(), conditions);
-
-			if( selectRsult.size() > 0 ) {
-				Hashtable<String, String> row;
-
-				for(int i = 0; i < selectRsult.size(); i++) {
-					row = selectRsult.get(i);
-
-					result.add(new Course(Integer.parseInt(row.get("id")), row.get("name")));
-				}
-			}
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-
-		return result;
+	public static ArrayList<Course> getWhere(String key, String value, boolean use_like) throws ClassNotFoundException, SQLException {
+		CourseDAO cDB = new CourseDAO();
+		return cDB.getCourseWhere(key, value, use_like);
 	}
 }
