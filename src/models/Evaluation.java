7b4a20557ad71f45a8abf035c7d1b1245223bf2a
Jonnatas Lennon Lima Costa <jonatas_lenon@hotmail.com>
2014-04-23 11:16:53 -0300

    Creating model class Evaluation

--- src/models/Evaluation.java
+++ src/models/Evaluation.java
@@ -5,163 +5,70 @@ import java.util.ArrayList;
 
 public class Evaluation extends Bean {
 
-	private int id;
-	private int id_institution;
-	private int year;
+	private Integer id;
+	private Integer idInstitution;
+	private Integer idCourse;
+	private Integer year;
 	private String modality;
-	private int master_degree_start_year;
-	private int doctorate_start_yaear;
-	private int triennial_evaluation;
-	private int permanent_teachers;
-	private int theses;
-	private int dissertations;
-	private int id_articles;
-	private int id_books;
-	private int published_works;
-	private int artistic_production;
+	private Integer masterDegreeStartYear;
+	private Integer doctorateStartYear;
+	private Integer triennialEvaluation;
+	private Integer permanentTeachers;
+	private Integer theses;
+	private Integer dissertations;
+	private Integer idArticles;
+	private Integer idBooks;
+	private Integer publishedWorks;
+	private Integer artisticProduction;
 
-	public Evaluation() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public Evaluation(int id, int id_institution, int year, String modality,
-			int master_degree_start_year, int doctorate_start_yaear,
-			int triennial_evaluation, int permanent_teachers, int theses,
-			int dissertations, int id_articles, int id_books,
-			int published_works, int artistic_production) {
-		
-		this.id = 0;
-		this.id_institution = id_institution;
-		this.year = year;
-		this.modality = modality;
-		this.master_degree_start_year = master_degree_start_year;
-		this.doctorate_start_yaear = doctorate_start_yaear;
-		this.triennial_evaluation = triennial_evaluation;
-		this.permanent_teachers = permanent_teachers;
-		this.theses = theses;
-		this.dissertations = dissertations;
-		this.id_articles = id_articles;
-		this.id_books = id_books;
-		this.published_works = published_works;
-		this.artistic_production = artistic_production;
-	}
 	
+	public Evaluation() {
+		this.id = 0;
+		this.identifier = "evaluation";
+		this.relationship = "";
 
-	public int getId() {
-		return id;
 	}
-
-	public void setId(int id) {
+	public Evaluation(Integer id) {
 		this.id = id;
-	}
-
-	public int getId_institution() {
-		return id_institution;
-	}
-
-	public void setId_institution(int id_institution) {
-		this.id_institution = id_institution;
-	}
-
-	public int getYear() {
-		return year;
-	}
-
-	public void setYear(int year) {
-		this.year = year;
-	}
-
-	public String getModality() {
-		return modality;
-	}
-
-	public void setModality(String modality) {
-		this.modality = modality;
-	}
-
-	public int getMaster_degree_start_year() {
-		return master_degree_start_year;
-	}
-
-	public void setMaster_degree_start_year(int master_degree_start_year) {
-		this.master_degree_start_year = master_degree_start_year;
-	}
-
-	public int getDoctorate_start_yaear() {
-		return doctorate_start_yaear;
-	}
-
-	public void setDoctorate_start_yaear(int doctorate_start_yaear) {
-		this.doctorate_start_yaear = doctorate_start_yaear;
-	}
-
-	public int getTriennial_evaluation() {
-		return triennial_evaluation;
-	}
-
-	public void setTriennial_evaluation(int triennial_evaluation) {
-		this.triennial_evaluation = triennial_evaluation;
-	}
-
-	public int getPermanent_teachers() {
-		return permanent_teachers;
-	}
-
-	public void setPermanent_teachers(int permanent_teachers) {
-		this.permanent_teachers = permanent_teachers;
-	}
-
-	public int getTheses() {
-		return theses;
-	}
-
-	public void setTheses(int theses) {
-		this.theses = theses;
-	}
-
-	public int getDissertations() {
-		return dissertations;
-	}
-
-	public void setDissertations(int dissertations) {
-		this.dissertations = dissertations;
-	}
-
-	public int getId_articles() {
-		return id_articles;
-	}
-
-	public void setId_articles(int id_articles) {
-		this.id_articles = id_articles;
-	}
-
-	public int getId_books() {
-		return id_books;
-	}
-
-	public void setId_books(int id_books) {
-		this.id_books = id_books;
-	}
-
-	public int getPublished_works() {
-		return published_works;
-	}
-
-	public void setPublished_works(int published_works) {
-		this.published_works = published_works;
-	}
-
-	public int getArtistic_production() {
-		return artistic_production;
-	}
-
-	public void setArtistic_production(int artistic_production) {
-		this.artistic_production = artistic_production;
-	}
-
+		this.identifier = "evaluation";
+		this.relationship = "";
+	}	
+	
+	
 	@Override
 	public String get(String field) {
-		// TODO Auto-generated method stub
+		if( field.equals("id")){
+			return Integer.toString(this.getId());
+		}else if( field.equals("id_institution")){
+			return Integer.toString(this.getIdInstitution());
+		}else if( field.equals("id_course")){
+			return Integer.toString(this.getIdCourse());
+		}else if( field.equals("year")){
+			return Integer.toString(this.getYear());
+		}else if( field.equals("modality")){
+			return getModality();
+		}else if( field.equals("master_degree_start_year")){
+			return Integer.toString(this.getMasterDegreeStartYear());
+		}else if( field.equals("doctorate_start_year")){
+			return Integer.toString(this.getDoctorateStartYear());
+		}else if( field.equals("triennial_evaluation")){
+			return Integer.toString(this.getTriennialEvaluation());
+		}else if( field.equals("permanent_teachers")){
+			return Integer.toString(this.getPermanentTeachers());
+		}else if( field.equals("theses")){
+			return Integer.toString(this.getTheses());
+		}else if( field.equals("dissertations")){
+			return Integer.toString(this.getDissertations());
+		}else if( field.equals("id_articles")){
+			return Integer.toString(this.getIdArticles());
+		}else if( field.equals("id_books")){
+			return Integer.toString(this.getIdBooks());
+		}else if( field.equals("published_works")){
+			return Integer.toString(this.getPublishedWorks());
+		}else if( field.equals("artistic_production")){
+			return Integer.toString(this.getArtisticProduction());
+		}else
+		
 		return null;
 	}
 
@@ -173,8 +80,119 @@ public class Evaluation extends Bean {
 
 	@Override
 	public ArrayList<String> fieldsList() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<String> fields = new ArrayList<String>();
+		fields.add("id");
+		fields.add("id_institution");
+		fields.add("id_course");
+		fields.add("year");
+		fields.add("modality");
+		fields.add("master_degree_start_year");
+		fields.add("doctorate_start_year");
+		fields.add("triennial_evaluation");
+		fields.add("permanent_teachers");
+		fields.add("theses");
+		fields.add("dissertations");
+		fields.add("id_articles");
+		fields.add("id_books");
+		fields.add("published_works");
+		fields.add("artistic_production");
+		return fields;
 	}
+	
+	
+	
 
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getIdInstitution() {
+		return idInstitution;
+	}
+	public void setIdInstitution(Integer idInstitution) {
+		this.idInstitution = idInstitution;
+	}
+	
+	public Integer getIdCourse() {
+		return idCourse;
+	}
+	public void setIdCourse(Integer idCourse) {
+		this.idCourse = idCourse;
+	}
+	public Integer getYear() {
+		return year;
+	}
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+	public String getModality() {
+		return modality;
+	}
+	public void setModality(String modality) {
+		this.modality = modality;
+	}
+	public Integer getMasterDegreeStartYear() {
+		return masterDegreeStartYear;
+	}
+	public void setMasterDegreeStartYear(Integer masterDegreeStartYear) {
+		this.masterDegreeStartYear = masterDegreeStartYear;
+	}
+	public Integer getDoctorateStartYear() {
+		return doctorateStartYear;
+	}
+	public void setDoctorateStartYear(Integer doctorateStartYear) {
+		this.doctorateStartYear = doctorateStartYear;
+	}
+	public Integer getTriennialEvaluation() {
+		return triennialEvaluation;
+	}
+	public void setTriennialEvaluation(Integer triennialEvaluation) {
+		this.triennialEvaluation = triennialEvaluation;
+	}
+	public Integer getPermanentTeachers() {
+		return permanentTeachers;
+	}
+	public void setPermanentTeachers(Integer permanentTeachers) {
+		this.permanentTeachers = permanentTeachers;
+	}
+	public Integer getTheses() {
+		return theses;
+	}
+	public void setTheses(Integer theses) {
+		this.theses = theses;
+	}
+	public Integer getDissertations() {
+		return dissertations;
+	}
+	public void setDissertations(Integer dissertations) {
+		this.dissertations = dissertations;
+	}
+	public Integer getIdArticles() {
+		return idArticles;
+	}
+	public void setIdArticles(Integer idArticles) {
+		this.idArticles = idArticles;
+	}
+	public Integer getIdBooks() {
+		return idBooks;
+	}
+	public void setIdBooks(Integer idBooks) {
+		this.idBooks = idBooks;
+	}
+	public Integer getPublishedWorks() {
+		return publishedWorks;
+	}
+	public void setPublishedWorks(Integer publishedWorks) {
+		this.publishedWorks = publishedWorks;
+	}
+	public Integer getArtisticProduction() {
+		return artisticProduction;
+	}
+	public void setArtisticProduction(Integer artisticProduction) {
+		this.artisticProduction = artisticProduction;
+	}
+	
 }
