579cfce431515c3882a7dcc0d7e096c513012e64
Brenddon Gontijo <brenddongontijo@msn.com>
2014-04-24 14:24:17 -0300

    Changed method shouldCreateNewEvaluationOnDataBase on TestEvaluation.java

--- src/tests/models/TestEvaluation.java
+++ src/tests/models/TestEvaluation.java
@@ -54,6 +54,39 @@ public class TestEvaluation {
 		evaluation.setPublishedWorks(Integer.parseInt("4"));
 		evaluation.setArtisticProduction(Integer.parseInt("5"));
 		evaluation.save();
+		
+		institution = new Institution();
+		institution.setAcronym("2");
+		institution.save();
+		
+		course = new Course();
+		course.setName("name course 2");
+		course.save();
+		
+		article = new Article();
+		article.setInternationals(Integer.parseInt("2"));
+		article.save();
+		
+		book = new Book();
+		book.setIntegralText(Integer.parseInt("2"));
+		book.save();
+		
+		evaluation = new Evaluation();
+		evaluation.setIdInstitution(institution.getId());
+		evaluation.setIdCourse(course.getId());
+		evaluation.setYear(Integer.parseInt("2015"));
+		evaluation.setModality("modality 2");
+		evaluation.setMasterDegreeStartYear(Integer.parseInt("2001"));
+		evaluation.setDoctorateStartYear(Integer.parseInt("2011"));
+		evaluation.setTriennialEvaluation(Integer.parseInt("2004"));
+		evaluation.setPermanentTeachers(Integer.parseInt("2"));
+		evaluation.setTheses(Integer.parseInt("3"));
+		evaluation.setDissertations(Integer.parseInt("4"));
+		evaluation.setIdArticles(article.getId());
+		evaluation.setIdBooks(book.getId());
+		evaluation.setPublishedWorks(Integer.parseInt("5"));
+		evaluation.setArtisticProduction(Integer.parseInt("6"));
+		evaluation.save();
 	}
 
 	@AfterClass
@@ -71,23 +104,48 @@ public class TestEvaluation {
 		int initialCount = Evaluation.count();
 		
 		Institution institution = new Institution();
-		institution.setAcronym("acronym");
+		institution.setAcronym("1");
 		institution.save();
 		
 		Course course = new Course();
 		course.setName("name course");
 		course.save();
 		
+		Article article = new Article();
+		article.setInternationals(Integer.parseInt("1"));
+		article.save();
+		
+		Book book = new Book();
+		book.setIntegralText(Integer.parseInt("1"));
+		book.save();
+		
 		Evaluation evaluation = new Evaluation();
 		evaluation.setIdInstitution(institution.getId());
 		evaluation.setIdCourse(course.getId());
-		evaluation.setYear(Integer.parseInt("2000"));
+		evaluation.setYear(Integer.parseInt("2014"));
+		evaluation.setModality("modality");
+		evaluation.setMasterDegreeStartYear(Integer.parseInt("2000"));
+		evaluation.setDoctorateStartYear(Integer.parseInt("2010"));
+		evaluation.setTriennialEvaluation(Integer.parseInt("2003"));
+		evaluation.setPermanentTeachers(Integer.parseInt("1"));
+		evaluation.setTheses(Integer.parseInt("2"));
+		evaluation.setDissertations(Integer.parseInt("3"));
+		evaluation.setIdArticles(article.getId());
+		evaluation.setIdBooks(book.getId());
+		evaluation.setPublishedWorks(Integer.parseInt("4"));
+		evaluation.setArtisticProduction(Integer.parseInt("5"));
 		
 		assertEquals(true, evaluation.save());
 		assertEquals(initialCount, Evaluation.count()-1);
+		assertEquals("1", Institution.get(Evaluation.last().getIdInstitution()).getAcronym());
+		assertEquals("name course", Course.get(Evaluation.last().getIdCourse()).getName());
+		assertEquals("1", Article.get(Evaluation.last().getIdArticles()).getInternationals());
+		assertEquals("1", Book.get(Evaluation.last().getIdBooks()).getIntegralText());
 		
 		institution.delete();
 		course.delete();
+		book.delete();
+		article.delete();
 		evaluation.delete();
 	}
 	
